f8bfc2bcf82b32e229f696dec0cf3421396ba7c0
PetyoPopov <35403054+petyoPopov@users.noreply.github.com>
2019-12-16 16:25:20 +0100

    Update nnet_cnn.py
    better definition of the layers is added

--- project2/part2-mnist/nnet_cnn.py
+++ project2/part2-mnist/nnet_cnn.py
@@ -43,10 +43,17 @@ def main():
     #################################
     ## Model specification TODO
     model = nn.Sequential(
-              nn.Conv2d(1, 32, (3, 3)),
-              nn.ReLU(),
-              nn.MaxPool2d((2, 2)),
-            )
+          nn.Conv2d(1, 32, (3, 3)),
+          nn.ReLU(),
+          nn.MaxPool2d((2, 2)),
+          nn.Conv2d(32,64,(3,3)),
+          nn.ReLU(),
+          nn.MaxPool2d((2,2)),
+          Flatten(), #flatten Layer
+          nn.Linear(1600,128),
+          nn.Dropout(p=0.5), # drop layer p=0.5
+          nn.Linear(128,10)
+          )
     ##################################
 
     train_model(train_batches, dev_batches, model, nesterov=True)
